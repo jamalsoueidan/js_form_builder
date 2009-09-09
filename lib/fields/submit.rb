2647c8e04d2821cecde998d17e4b800283e02b2a
= <jamal@soueidan.com>
2009-09-09 13:03:55 +0200

    Changed hash options

--- lib/fields/submit.rb
+++ lib/fields/submit.rb
@@ -1,10 +1,10 @@
 class Submit < InputBaseClass
   def build
+    value = submit_tag_value
     if options.class.to_s == "String"
       value = options
-      options = {:label => value}
     end
-    submit_tag = template.submit_tag(submit_tag_value, :id => builder.object_name.to_s + "_submit")
+    submit_tag = template.submit_tag(value, :id => builder.object_name.to_s + "_submit")
     content_tag(:div, submit_tag, :class => "submit")
   end
   
@@ -12,9 +12,9 @@ class Submit < InputBaseClass
      def submit_tag_value
         value = "Save changes"
         if options[:label]
-          value = options[:label]
+          return options[:label]
         elsif options[:submit_label]
-          value = options[:submit_label]
+          return options[:submit_label]
         end 
         return value
       end
--- lib/fields/submit_or_cancel.rb
+++ lib/fields/submit_or_cancel.rb
@@ -1,26 +1,31 @@
 class SubmitOrCancel < InputBaseClass
   def build
-    submit_tag = template.submit_tag(submit_tag_value, :id => builder.object_name.to_s + "_submit")
-    cancel_link = template.cancel_link(cancel_link_value)
+    submit_tag = template.submit_tag(submit_value, :id => builder.object_name.to_s + "_submit")
+    cancel_link = template.link_to(cancel_label, cancel_link)
     both = content_tag(:div, submit_tag + " or " + cancel_link, :class => "submit_or_cancel")
     content_tag(:div, both, :class => "submit")
   end
   
   private
-    def submit_tag_value
+    def submit_value
       value = "Save changes"
-      if options[:label]
-        value = options[:label]
-      elsif options[:submit_label]
-        value = options[:submit_label]
-      end 
+      if options[:submit] && options[:submit][:label]
+        return options[:submit][:label]
+      end
       return value
     end
   
-    def cancel_link_value
-      if options[:cancel_label]
-        return options[:cancel_label]
+    def cancel_label
+      if options[:cancel] && options[:cancel][:label]
+        return options[:cancel][:label]
       end
       return "Cancel"
+    end
+    
+    def cancel_link
+      if options[:cancel]
+        return options[:cancel][:url]
+      end
+      return "/"
     end
 end
