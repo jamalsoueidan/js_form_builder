cc1a74459054b23d59b4ea11aa5b0f814b7a702f
= <jamal@soueidan.com>
2009-09-10 21:37:51 +0200

    Added boiler_plate plugin

--- init.rb
+++ init.rb
@@ -8,3 +8,13 @@ ActionView::Base.send(:include, JsActionView)
 
 # To remove <div class="fieldWithErrors"> around errors.
 ActionView::Base.field_error_proc = proc { |input, instance| input } 
+
+
+# validation_reflection plugin
+require 'boiler_plate/validation_reflection'
+
+ActiveRecord::Base.class_eval do
+  include BoilerPlate::ActiveRecordExtensions::ValidationReflection
+  BoilerPlate::ActiveRecordExtensions::ValidationReflection.load_config
+  BoilerPlate::ActiveRecordExtensions::ValidationReflection.install(self)
+end
