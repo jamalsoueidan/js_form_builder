5fb4b435968127057bc01339635ab890883274e5
Jamal Soueidan <jamal@soueidan.com>
2010-05-18 13:26:02 +0200

    Updated check_box options

--- lib/fields/check_box.rb
+++ lib/fields/check_box.rb
@@ -1,7 +1,12 @@
 class CheckBox < InputBaseClass
   def build
-    text_beside_check_box = content_tag(:label, check_box_value, :for => builder.object_name.to_s + "_" + input_name.to_s, :style => "cursor:pointer")
-    content = content_tag(:div, input_tag + " " + text_beside_check_box, :class => "label")
+    text_box = content_tag(:label, check_box_value, :for => builder.object_name.to_s + "_" + input_name.to_s, :style => "cursor:pointer")
+    value = input_tag + " " + text_box
+    if options[:set_label_before] == true
+      value = text_box + " " + input_tag
+    end
+    
+    content = content_tag(:div, value, :class => "label")
     content_tag(:div, content, :class => "row")
   end
   
